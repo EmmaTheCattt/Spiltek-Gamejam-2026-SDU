344b1ab745988015b46fb50e8106b7350ebded0b
Lasse Smidsgaard Andersen <Lasseandersen006@gmail.com>
2026-05-01 19:56:25 +0200

    Audio init

---
+++ Purple/Assets/SCRIPTS/LASSE AUDIO/AudioManager.cs
@@ -0,0 +1,142 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private Sound[] sounds;
+    public static AudioManager instance;
+
+    [SerializeField] private Sound[] walkSounds;
+    [SerializeField] private Sound[] slimeSounds; 
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+            s.source.pitch = s.pitch;
+            s.source.playOnAwake = s.PlayOnAwake;
+        }
+
+        foreach(Sound s in walkSounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+            s.source.pitch = s.pitch;
+            s.source.playOnAwake = s.PlayOnAwake;
+        }
+
+        foreach(Sound s in slimeSounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+            s.source.pitch = s.pitch;
+            s.source.playOnAwake = s.PlayOnAwake;
+        }
+
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            print("Sound: " + name + " not found");
+            return;
+        }
+
+        s.source.Play();
+    }
+
+    public void StopMusic(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            print("Sound: " + name + " not found");
+            return;
+        }
+        s.source.Stop();
+    }
+
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            print("Sound: " + name + " not found");
+            return;
+        }
+
+        s.source.Play();
+    }
+
+    public void StopSFX(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            print("Sound: " + name + " not found");
+            return;
+        }
+
+        s.source.Stop();
+    }
+
+    public IEnumerator WalkingLoop()
+    {
+        while (true)
+        {
+            Sound walkSound = Array.Find(walkSounds, sound => sound.name == "Walk");
+
+            walkSound.source.pitch = UnityEngine.Random.Range((float)0.8, 1.2f);
+            PlaySFX(walkSound.name);
+
+            float delay = 0.35f + UnityEngine.Random.Range((float)-0.05f, 0.08f);
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+    public void PlayRandomSlimeSFX()
+    {
+        int lastIndex = -1;
+        int randomIndex;
+        do
+        {
+            randomIndex = UnityEngine.Random.Range(0, slimeSounds.Length);
+        } while (randomIndex == lastIndex);
+        var randomSlimeSound = slimeSounds[randomIndex];
+        randomSlimeSound.source.Play();
+        Debug.Log("Playing slime sound");
+    }
+}
