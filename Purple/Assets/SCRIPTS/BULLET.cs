89af88e3f8e1b7ef5fb507261f75d6364e55b7d6
Lasse Smidsgaard Andersen <Lasseandersen006@gmail.com>
2026-05-02 17:35:58 +0200

    jump removed

--- Purple/Assets/SCRIPTS/BULLET.cs
+++ Purple/Assets/SCRIPTS/BULLET.cs
@@ -12,7 +12,6 @@ public class BULLET : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        AudioManager.instance.PlaySFX("Jumo");
         AudioManager.instance.PlaySFX("Shot");
     }
 
